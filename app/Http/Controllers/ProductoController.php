758c9d505495c827663d5d89f054bdc73be39fff
fray david herrera duarte <148037427+Frayherrera@users.noreply.github.com>
2025-06-16 23:47:06 -0500

    cambio2

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Producto;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 
 
 use Illuminate\Http\Request;
@@ -63,8 +64,8 @@ class ProductoController extends Controller
 
             // ✅ Log de éxito
             Log::channel('personal')->info('Producto registrado', [
+                'usuario_id' => Auth::id(),
                 'producto_id' => $producto->id,
-                'usuario_id' => auth()->id(),
                 'codigo_barras' => $producto->codigo_barras,
                 'nombre' => $producto->nombre,
             ]);
@@ -75,7 +76,7 @@ class ProductoController extends Controller
             // ❌ Log de error
             Log::channel('personal')->error('Error al registrar producto', [
                 'error' => $e->getMessage(),
-                'usuario_id' => auth()->id(),
+                'usuario_id' => Auth::id(),
                 'request_data' => $request->all(),
             ]);
 
@@ -139,7 +140,7 @@ class ProductoController extends Controller
             $producto->delete();
             Log::channel('personal')->info('Producto eliminado', [
                 'producto_id' => $producto->id,
-                'usuario_id' => auth()->id(),
+                'usuario_id' => Auth::id(),
                 'codigo_barras' => $producto->codigo_barras,
                 'nombre' => $producto->nombre,
             ]);
